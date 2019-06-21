087f401b5691c81e57a934594989425beee3e4cf
Kayli Leung <kay.schulz10@gmail.com>
2019-06-21 11:26:28 -0700

    get flask app to run

--- instagram_flask/app.py
+++ instagram_flask/app.py
@@ -6,25 +6,25 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 import h5py
 from flask import Flask, request, render_template, jsonify
-import tensorflow as tf
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 from nltk import word_tokenize
-from keras.preprocessing.sequence import pad_sequences
-from keras.preprocessing import sequence
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing import sequence
 import pickle
+import tensorflow as tf
 
 import os
-os.environ['KERAS_BACKEND'] = 'theano'
+#os.environ['KERAS_BACKEND'] = 'theano'
 import keras as ks
 
-model = tf.keras.models.load_model("app_model.h5")
-tokenizer = pickle.load(open('tokenizer.pkl', 'rb'))
+# model = load_model("instagram_flask/final_model.hdf5")
+tokenizer = pickle.load(open('instagram_flask/tokenizer.pkl', 'rb'))
 app = Flask(__name__, static_url_path="")
-graph = tf.get_default_graph()
+#graph = tf.keras.get_default_graph()
 
 def get_model():
     global model
-    model = tf.keras.models.load_model("app_model.h5")
+    model = load_model("instagram_flask/final_model.hdf5")
     
 @app.route('/')
 def index():
@@ -34,21 +34,22 @@ def index():
 # vectorizes and tokenizes the input
 
 def tokenize_input(user_input):
-    """Converts input text to word vectors for model"""
-    words = word_tokenize(user_input)
-    list_tokenized = tokenizer.texts_to_sequences(words)
+    """Vectorize and tokenize input"""
+    list_tokenized = tokenizer.texts_to_sequences(user_input.map(word_tokenize).values)
     X_t = sequence.pad_sequences(list_tokenized, maxlen=100)
     return X_t
 
 get_model()
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET','POST'])
 def predict():
     """Return a random prediction."""
     data = request.get_json(force=True)
-    X_t = tokenize_input(data['user_input'])
-    prediction = model.predict(X_t).to_list()
-    response = {'probability not promo': prediction[0][0].round(4),
-                'probability_promo': prediction[0][1].round(4)}
+    user_input = pd.Series(data, name='comment')
+    X_t = tokenize_input(user_input)
+    prediction = model.predict(X_t)
+    response = {'probability not promo': str(prediction[0][0].round(4)),
+                'probability_promo': str(prediction[0][1].round(4))}
     return jsonify(response)
+
     
